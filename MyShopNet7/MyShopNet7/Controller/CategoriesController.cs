01756318d68b55c97e8d6223970bf875380b4daf
KuroNgo <hoaiphong01012002@gmail.com>
2023-05-25 22:57:05 +0700

    Update Code C#

---
+++ MyShopNet7/MyShopNet7/Controller/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyShopNet7.Entities;
+
+namespace MyShopNet7.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly MyshopContext _context;
+        public CategoriesController(MyshopContext ctx)
+        {
+            _context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Categories.ToList());
+        }
+    }
+}
---
+++ MyShopNet7/MyShopNet7/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using MyShopNet7.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyshopContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyShop")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
